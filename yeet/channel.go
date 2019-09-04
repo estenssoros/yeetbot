7cbd111dbd553f98efcf3615abef5bda42265d0b
estenssoros <seb.estenssoro@gmail.com>
2019-09-03 20:33:30 -0600

    channel cmd

---
+++ yeet/channel.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/estenssoros/yeetbot/client"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	channelCmd.AddCommand(channelListCmd)
+	channelCmd.AddCommand(channelPurgeCmd)
+}
+
+var channelCmd = &cobra.Command{
+	Use:   "channel",
+	Short: "do channel stuff",
+}
+
+var channelListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list channels",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := client.NewAWS()
+		if err != nil {
+			return errors.Wrap(err, "new aws")
+		}
+		channels, err := c.ListChannels()
+		if err != nil {
+			return errors.Wrap(err, "client list channels")
+		}
+		for _, channel := range channels {
+			fmt.Println(channel.ID, channel.Name)
+		}
+		return nil
+	},
+}
+
+var channelPurgeCmd = &cobra.Command{
+	Use:   "purge",
+	Short: "purge all messages from a channel",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("must supply channel argument")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := client.NewAWS()
+		if err != nil {
+			return errors.Wrap(err, "client new aws")
+		}
+		messages, err := c.ListMessages(args[0])
+		if err != nil {
+			return errors.Wrap(err, "client list messages")
+		}
+		toDelete := map[string]string{}
+		for _, message := range messages {
+			if err := c.DeleteBotMessage(args[0], message.Ts); err != nil {
+				toDelete[message.Ts] = message.Text
+			}
+		}
+		for messageTS := range toDelete {
+			if err := c.DeleteUserMessage(args[0], messageTS); err == nil {
+				delete(toDelete, messageTS)
+			}
+		}
+		if len(toDelete) > 0 {
+			fmt.Println("failed to delete messages")
+		}
+		for messageTS, messageText := range toDelete {
+			fmt.Println(messageTS, messageText)
+		}
+		return nil
+	},
+}
