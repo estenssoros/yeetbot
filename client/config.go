736e27010cbb91ad40df61ab8d3a5d53abf832a5
estenssoros <seb.estenssoro@gmail.com>
2019-09-11 16:49:22 -0600

    Config

---
+++ client/config.go
@@ -0,0 +1,22 @@
+package client
+
+import "encoding/json"
+
+type Config struct {
+	UserToken string    `json:"user_token"`
+	BotToken  string    `json:"bot_token"`
+	Reports   []*Report `json:"reports"`
+}
+
+func (c Config) String() string {
+	ju, _ := json.MarshalIndent(c, "", " ")
+	return string(ju)
+}
+
+func (c *Config) NewClient(report *Report) *Client {
+	return &Client{
+		UserToken: c.UserToken,
+		BotToken:  c.BotToken,
+		Report:    report,
+	}
+}
