265ab5966e44b391c3ba1020ed9e0a7d25ad8f6d
estenssoros <seb.estenssoro@gmail.com>
2019-09-11 19:10:24 -0600

    read in config and usermap

--- client/config.go
+++ client/config.go
@@ -1,7 +1,20 @@
 package client
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
 
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	yeetENV = "YEETBOT_CONFIG"
+)
+
+// Config all info for a yeetbot config
 type Config struct {
 	UserToken string    `json:"user_token"`
 	BotToken  string    `json:"bot_token"`
@@ -13,10 +26,54 @@ func (c Config) String() string {
 	return string(ju)
 }
 
+// NewClient creates a report client from a config
 func (c *Config) NewClient(report *Report) *Client {
 	return &Client{
 		UserToken: c.UserToken,
 		BotToken:  c.BotToken,
 		Report:    report,
 	}
+}
+
+// ConfigFromReader creates a config from a reader
+func ConfigFromReader(r io.Reader) (*Config, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "read reader")
+	}
+	config := &Config{}
+	if err := yaml.Unmarshal(data, config); err != nil {
+		return nil, errors.Wrap(err, "yaml unmarshal")
+	}
+	return config, nil
+}
+
+// ConfigFromEnv loads config from an environment variable
+func ConfigFromEnv() (*Config, error) {
+	path := os.Getenv(yeetENV)
+	if path == "" {
+		return nil, errors.New("missing " + yeetENV)
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "open file")
+	}
+	defer f.Close()
+	return ConfigFromReader(f)
+}
+
+// UserMap loops through all reports and users to create a user map to reports
+func (c *Config) UserMap() *UserMap {
+	userMap := UserMap(map[string][]*Report{})
+	for _, report := range c.Reports {
+		for _, u := range report.Users {
+			if reports, ok := userMap[u.Name]; !ok {
+				userMap[u.Name] = []*Report{report}
+			} else {
+				reports = append(reports, report)
+				userMap[u.Name] = reports
+			}
+		}
+	}
+	return &userMap
 }
