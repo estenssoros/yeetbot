3e0e5ca4607041b849c2c8bfa985a977c76965a0
estenssoros <seb.estenssoro@gmail.com>
2019-09-13 21:24:13 -0600

    how me thinks event go

--- views/event.go
+++ views/event.go
@@ -1,68 +1,74 @@
 package views
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/estenssoros/yeetbot/client"
 	"github.com/estenssoros/yeetbot/slack"
+	"github.com/labstack/echo"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
-func EventHandler(c client.Context) error {
+func EventHandler(c echo.Context) error {
 	req := &slack.EventRequest{}
 	if err := c.Bind(req); err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
+
 	// slack requires that a URL be validated with a "challenge"
 	if req.Challenge != "" {
 		return c.JSON(http.StatusOK, req.Challenge)
 	}
 
-	// what report is this from (maybe from report channel?)
+	// what report is this from?
+	cli, err := c.(*client.Context).Config.NewClientFromChannel(req.Event.Channel)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
 	// what user sent this report?
+	user, err := cli.GetUserByID(req.Event.User)
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	if req.Event.Text == "report" && !cli.HasUserStartedReport(user) {
+		if err := cli.InitiateReport(user); err != nil {
+			return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client initiate report"))
+		}
+		return c.NoContent(http.StatusOK)
+	}
+
 	// what stage is this user at?
-	// record response
+	question, err := cli.GetRecentQuestion(user)
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client get last question"))
+	}
+
+	// record a response for a step OR overwrite a response for a step
+	err = cli.RecordResponse(&client.RecordResponseInput{
+		Question: question,
+		User:     user,
+		Text:     req.Event.Text,
+	})
+
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client record response"))
+	}
+
+	// report is complete! let's do this!
+	if cli.IsReportComplete(user) {
+		return errors.Wrap(cli.CompleteReport(user), "client complete report")
+	}
+
 	// initiate next stage
-
-	client := c.Config.NewClient(&client.Report{})
-	user, err := client.GetUserFromRequest(req)
-	if err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusInternalServerError, err)
+	if err := cli.PostNextQuestion(user); err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client post next question"))
 	}
-	report, err := client.FindReportByUser(user, c.UserReports)
-	if err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-	client.Report = report
-	response, err := client.GetUserResponse(user)
-	if err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-	if response == nil {
-		if err := client.PostFirstQuestion(user); err != nil {
-			log.Println(err)
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-		c.JSON(http.StatusOK, req.Challenge)
-	}
-	client.Response = response
-	total, err := client.RecordResponse(user, req.Event.Text)
-	if err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-	if total == len(client.Report.Questions) {
-		if err := client.PostNextQuestion(user); err != nil {
-			log.Println(err)
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-		c.JSON(http.StatusOK, req.Challenge)
-	}
-	client.CompleteResponse(user)
-	// send complete message to report channel
-
 	return c.NoContent(http.StatusOK)
 }
