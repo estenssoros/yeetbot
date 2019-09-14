8a3b1d32337eaac0eb7f55a910e05305802eea35
estenssoros <seb.estenssoro@gmail.com>
2019-09-13 21:23:30 -0600

    record response input

--- client/response.go
+++ client/response.go
@@ -17,14 +17,21 @@ type Response struct {
 	Responses       []string  `json:"responses"`
 }
 
-// GetResponsesByUser gets user response from elastic search
+type RecordResponseInput struct {
+	Question *Question
+	User     *slack.User
+	Text     string
+}
+
+// GetUserResponse gets user response from elastic search
+// list most recent responses on channel
 func (c *Client) GetUserResponse(user *slack.User) (*Response, error) {
 	return &Response{}, nil
 }
 
 // RecordResponse adds response to responses and returns total number of responses recorded
-func (c *Client) RecordResponse(user *slack.User, message string) (int, error) {
-	return 0, nil
+func (c *Client) RecordResponse(input *RecordResponseInput) error {
+	return nil
 }
 
 // CompleteResponse removes pending status from response and sends "thank you" message to user
