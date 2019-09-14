08831e87c66ba5bb950477dcdf235735b6582853
estenssoros <seb.estenssoro@gmail.com>
2019-09-14 15:05:57 -0600

    new client from user

--- client/context.go
+++ client/context.go
@@ -1,9 +1,11 @@
 package client
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/labstack/echo"
+	"github.com/pkg/errors"
 )
 
 // Context wrapper around echo's context
@@ -29,7 +31,17 @@ func Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 func (c *Context) populateUserReports() {
 	for _, report := range c.Config.Reports {
 		for _, user := range report.Users {
-			c.UserReports[user.Name] = append(c.UserReports[user.Name], report)
+			c.UserReports[user.ID] = append(c.UserReports[user.Name], report)
 		}
 	}
+}
+
+func (c *Context) NewClientFromUser(userID string) (*Client, error) {
+	reports, ok := c.UserReports[userID]
+	if !ok {
+		fmt.Println(c.Config)
+		return nil, errors.Errorf("could not locate user: %s", userID)
+	}
+	// TODO how do we find the right report?
+	return c.Config.NewClient(reports[0]), nil
 }
