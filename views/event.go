ec4564e1c0f4cdadc10423fe1cae6d93da9d28c7
estenssoros <seb.estenssoro@gmail.com>
2019-09-15 20:54:30 -0600

    phew

--- views/event.go
+++ views/event.go
@@ -1,15 +1,12 @@
 package views
 
 import (
-	"context"
 	"net/http"
-	"time"
 
 	"github.com/estenssoros/yeetbot/client"
 	"github.com/estenssoros/yeetbot/slack"
 	"github.com/labstack/echo"
 	"github.com/pkg/errors"
-	"github.com/seaspancode/services/elasticsvc"
 	"github.com/sirupsen/logrus"
 )
 
@@ -43,112 +40,54 @@ func EventHandler(c echo.Context) error {
 		return c.JSON(http.StatusOK, req.Challenge)
 	}
 
-	logrus.Infof("event type: %s", req.Type)
+	cli := c.(*client.Context).NewEmptyClient()
 
-	// what report is this from?
-	cli, err := c.(*client.Context).NewClientFromUser(req.Event.User)
-	if err != nil {
-		logrus.Error(errors.Wrap(err, "new client from channel"))
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	es := elasticsvc.New(context.Background())
-
-	messages, err := cli.ListTodayMessages(req.Event.Channel)
-	if err != nil {
-		logrus.Error(errors.Wrap(err, "cli list messages"))
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	// msgIdx, err := getMessageIndexByTS(messages, req.Event.Ts)
-
-	// did user ask for report shortcut
-	started, err := cli.HasUserStartedReport()
+	user, err := cli.GetUserByID(req.Event.User)
 	if err != nil {
 		logrus.Error(err)
 		return c.JSON(http.StatusInternalServerError, err)
 	}
-	if req.Event.Text == "report" && !started {
-		if err := cli.InitiateReport(); err != nil {
-			return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client initiate report"))
+
+	activeMeetings, err := cli.GetActiveMeetingsForUser(user)
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	meeting := activeMeetings[0]
+
+	for _, m := range activeMeetings {
+		if m.ScheduledStart.After(meeting.ScheduledStart) {
+			meeting = m
 		}
-		return c.NoContent(http.StatusOK)
 	}
 
-	// what stage is this user at?
-	question, err := cli.GetRecentQuestion()
-
+	lastMessage, err := cli.GetLastYeetQuestion(req.Event.Channel)
+	if err != nil {
+		logrus.Error(err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	report, err := cli.GetOrCreateUserReport(meeting, user)
 	if err != nil {
 		logrus.Error(err)
 		return c.JSON(http.StatusInternalServerError, err)
 	}
 
-	//question, err := getMessageByUserID(message[msgIdx:], cli.YeetUser)
+	event := &client.Event{
+		Question: lastMessage.Text,
+		Response: req.Event.Text,
+		TS:       req.Event.EventTs,
+	}
 
-	// record a response for a step OR overwrite a response from a previous stage
-	err = cli.RecordResponse(&client.RecordResponseInput{
-		Question: question,
-		User:     cli.YeetUser,
-		EventTS:  req.Event.EventTs,
-		Text:     req.Event.Text,
-	})
+	report.AddEvent(event)
 
-	if err != nil {
+	if err := cli.SaveReport(report); err != nil {
 		logrus.Error(err)
 		return c.JSON(http.StatusInternalServerError, err)
 	}
-
-	doc := &client.Response{
-		Team:     req.TeamID,
-		UserID:   req.Event.User,
-		EventTS:  req.Event.EventTs,
-		Date:     time.Now(),
-		Text:     req.Event.Text,
-		Question: question.Text,
-	}
-
-	if err := es.PutOne("yeetbot", doc); err != nil {
+	if err := cli.NextStage(meeting, lastMessage, user); err != nil {
 		logrus.Error(err)
-		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "es put one"))
+		return c.JSON(http.StatusInternalServerError, err)
 	}
-	logrus.Infof("added %s", doc.ID)
-
-	// // did user ask for report shortcut
-	// if req.Event.Text == "report" && !cli.HasUserStartedReport(user) {
-	// 	if err := cli.InitiateReport(user); err != nil {
-	// 		return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client initiate report"))
-	// 	}
-	// 	return c.NoContent(http.StatusOK)
-	// }
-
-	// // what stage is this user at?
-	// question, err := cli.GetRecentQuestion(user)
-	// if err != nil {
-	// 	logrus.Error(err)
-	// 	return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client get last question"))
-	// }
-
-	// // record a response for a step OR overwrite a response from a previous stage
-	// err = cli.RecordResponse(&client.RecordResponseInput{
-	// 	Question: question,
-	// 	User:     user,
-	// 	Text:     req.Event.Text,
-	// })
-
-	// if err != nil {
-	// 	logrus.Error(err)
-	// 	return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client record response"))
-	// }
-
-	// // report is complete! let's do this!
-	// if cli.IsReportComplete(user) {
-	// 	return errors.Wrap(cli.CompleteReport(user), "client complete report")
-	// }
-
-	// // initiate next stage
-	// if err := cli.PostNextQuestion(user); err != nil {
-	// 	logrus.Error(err)
-	// 	return c.JSON(http.StatusInternalServerError, errors.Wrap(err, "client post next question"))
-	// }
 	return c.NoContent(http.StatusOK)
 }
